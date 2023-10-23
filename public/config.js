48188ec73764c22a3d03e97d496a6a5c030af878
asbteama <148767411+asbteama@users.noreply.github.com>
2023-10-23 06:02:58 -0600

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "https://work.asbteama.workers.dev",
+  VITE_CORS_PROXY_URL: "https://hello.asbteama.workers.dev",
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMjVjN2Q4YzEyMzRkY2E4ZGUxM2M5MmNlMDJmNGM0ZCIsInN1YiI6IjY1MzY1NDM4YzE0ZmVlMDEwMGVhMWMzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.w5cUuRIfGS284QwBufjcZpaeiotf-nUh9aYwNaNVAHs"
 };
